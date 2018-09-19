8a90d5aa4d2e4a3d68df491e1a56582b0b74560c
akjena <39960046+akjena@users.noreply.github.com>
2018-09-19 10:14:36 -0400

    Updated to deploy in app server

--- src/main/java/com/akj/microservices/springcloudconfigserver/SpringCloudConfigServerApplication.java
+++ src/main/java/com/akj/microservices/springcloudconfigserver/SpringCloudConfigServerApplication.java
@@ -2,13 +2,21 @@ package com.akj.microservices.springcloudconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
 @SpringBootApplication
-public class SpringCloudConfigServerApplication {
+public class SpringCloudConfigServerApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCloudConfigServerApplication.class, args);
 	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+			return builder.sources(SpringCloudConfigServerApplication.class);
+	}
+
 }
